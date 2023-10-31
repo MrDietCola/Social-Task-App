814354a8ee5224494699793a5f400e45eb9e6624
Liam Peachey <liamjpeachey@gmail.com>
2023-10-31 13:35:12 -0500

    Added seed data for comments, tags, tasks, task-tags

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,5 +1,5 @@
 const sequelize = require('../config/connection');
-const { User } = require('../models');
+const { User, Task, Comments } = require('../models');
 
 const userData = require('./userData.json');
 // const projectData = require('./projectData.json');
@@ -11,6 +11,27 @@ const seedDatabase = async () => {
     individualHooks: true,
     returning: true,
   });
+
+  const tasks = await Task.bulkCreate(require('./taskData.json'), {
+    individualHooks: true,
+    returning: true,
+  });
+
+  const comments = await Comments.bulkCreate(require('./commentData.json'), {
+    individualHooks: true,
+    returning: true,
+  })
+
+  const tags = await Tags.bulkCreate(require('./tagData.json'), {
+    individualHooks: true,
+    returning: true,
+  })
+
+  const taskTags = await taskTags.bulkCreate(require('./taskTagData.json'), {
+    individualHooks: true,
+    returning: true,
+  })
+
 
   // for (const project of projectData) {
   //   await Project.create({
