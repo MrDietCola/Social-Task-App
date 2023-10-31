e26a2a3e2f55cb66ca838f5bad85768605e3ed56
Ivan Lopez <84734876+lopezivan763@users.noreply.github.com>
2023-10-31 14:37:51 -0500

    added third party api and display on landing handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,8 +1,32 @@
 const router = require('express').Router();
 const { User } = require('../models');
 const withAuth = require('../utils/auth');
+const axios = require('axios');
+
+const options = {
+  method: 'GET',
+  url: 'https://quotes15.p.rapidapi.com/quotes/random/',
+  headers: {
+    'X-RapidAPI-Key': '1715b24a43msh9b5023d4aa71c9bp108651jsn692826a37b09',
+    'X-RapidAPI-Host': 'quotes15.p.rapidapi.com'
+  }
+};
 
 router.get('/', async (req, res) => {
+// third party api route
+  try {
+    const response = await axios.request(options);
+    const quote = response.data.content;
+
+    res.render('landingPage', {
+      layout: 'landing.handlebars',
+      quote: quote
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+
   try {
     // Get all projects and JOIN with user data
     // const projectData = await Project.findAll({
@@ -77,5 +101,11 @@ router.get('/login', (req, res) => {
 
   res.render('login');
 });
+
+
+
+
+
+
 
 module.exports = router;
