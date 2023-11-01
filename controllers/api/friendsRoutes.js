4080788735adc33ee3e98ed3bf65bda0f1f3061f
Ivan Lopez <84734876+lopezivan763@users.noreply.github.com>
2023-11-01 15:10:25 -0500

    css for tasks, freinds handlebars and routes

--- controllers/api/friendsRoutes.js
+++ controllers/api/friendsRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Friends } = require('../../models');
+const { Friends, Task, User } = require('../../models');
 
 // CREATE one friend
 router.post('/', async (req, res) => {
@@ -11,7 +11,7 @@ router.post('/', async (req, res) => {
   }
 });
 
-// delete a friend
+// Delete a friend
 router.delete('/:id', async (req, res) => {
   try {
     const friendsData = await Friends.destroy({
@@ -24,11 +24,30 @@ router.delete('/:id', async (req, res) => {
       res.status(404).json({ message: 'No friends found with that id!' });
       return;
     }
-
     res.status(200).json(friendsData);
   } catch (err) {
     res.status(500).json(err);
   }
 });
+
+// Render friend tasks
+router.get('/friends', async (req, res) => {
+  try {
+    const friend = await Friends.findAll(req.params.id, {
+      include: {
+        model: User,
+        include: Task,
+        foreignKey: friend_id,
+      },
+    });
+    res.render('friendTasks', { friend });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server Error');
+  }
+});
+
+
+
 
 module.exports = router;
